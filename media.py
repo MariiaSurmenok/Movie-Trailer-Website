99ff5328e24166c25fd6319da37cd94966487f06
Mariia <mariasurmenok@gmail.com>
2017-01-27 18:23:38 -0800

    added comments to Movie class

--- media.py
+++ media.py
@@ -1,4 +1,13 @@
 class Movie():
+    """
+    Stores information of a movie.
+
+    Attributes:
+        title (str): title of a movie
+        poster_image_url (str): url of a poster
+        trailer_youtube_url (str): url of a trailer on youtube
+    """
+    
     def __init__(self, movie_title,
                  poster_image,trailer_youtube):
         self.title = movie_title
