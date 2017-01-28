6a7bfd168404f6509abd2a5258f6248d5f7383f0
Mariia <mariasurmenok@gmail.com>
2017-01-28 13:40:21 -0800

    meet pep8 style guide

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,7 +3,7 @@ import fresh_tomatoes
 
 # Create instances of class media.Movie(title, poster, trailer)
 rock_n_rolla = media.Movie("RocknRolla",
-                         "https://miguelvaca.files.wordpress.com/2009/11/rocknrolla.jpg", # NOQA
+                         "https://miguelvaca.files.wordpress.com/2009/11/rocknrolla.jpg",  # NOQA
                          "https://youtu.be/WEIykbkkq6I")
 
 the_martian = media.Movie("The Martian",
@@ -11,22 +11,22 @@ the_martian = media.Movie("The Martian",
                           "https://youtu.be/ej3ioOneTy8")
 
 ex_machina = media.Movie("Ex Machina",
-                         "http://cfile28.uf.tistory.com/image/221E454F575D908E22FD90", # NOQA
+                         "http://cfile28.uf.tistory.com/image/221E454F575D908E22FD90",  # NOQA
                          "https://youtu.be/45SUqUXpwgY")
 
 pacific_rim = media.Movie("Pacific Rim",
-                          "http://www.impawards.com/2013/posters/pacific_rim_ver12_xlg.jpg",
+                          "http://www.impawards.com/2013/posters/pacific_rim_ver12_xlg.jpg",  # NOQA
                           "https://youtu.be/5guMumPFBag")
 
 the_man = media.Movie("The Man from U.N.C.L.E.",
-                      "http://www.movieplayhit.com/wp-content/uploads/2015/07/The-Man-from-U.N.C.L.E..jpg", # NOQA
+                      "http://www.movieplayhit.com/wp-content/uploads/2015/07/The-Man-from-U.N.C.L.E..jpg",  # NOQA
                       "https://youtu.be/w_Ky4KPzKwY")
 
 guardians = media.Movie("Guardians of the Galaxy",
-                        "http://1.media.dorkly.cvcdn.com/26/95/18b149286ca6f2920e017bd5d2ffcbf5.jpg", # NOQA
+                        "http://1.media.dorkly.cvcdn.com/26/95/18b149286ca6f2920e017bd5d2ffcbf5.jpg",  # NOQA
                         "https://youtu.be/d96cjJhvlMA")
 
-# Add instances of Movie to the list 
+# Add instances of Movie to the list
 movies = [rock_n_rolla, the_martian, ex_machina,
           pacific_rim, the_man, guardians]
 
--- media.py
+++ media.py
@@ -7,9 +7,9 @@ class Movie():
         poster_image_url (str): url of a poster
         trailer_youtube_url (str): url of a trailer on youtube
     """
-    
+
     def __init__(self, movie_title,
-                 poster_image,trailer_youtube):
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
